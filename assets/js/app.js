5ecfd15ea352192b240e0cc7a8d7afd67f6ba4a7
Islam Askar <islam.askar@gmail.com>
2019-07-06 18:50:31 +0200

    Add bootstrap and jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,12 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+import $ from 'jquery';
+
+import 'bootstrap';
+
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
